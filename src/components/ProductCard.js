fd18349aca5dfc46ba7d6a7ec2120a464f9a03f2
RiTiK <168961238+ritikkumar8z@users.noreply.github.com>
2025-08-17 03:19:51 +0530

    Update ProductCard.js

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -8,7 +8,7 @@ import {
     TouchableOpacity, 
     View,
 } from 'react-native'; 
- 
+  
 const { width } = Dimensions.get('window');
 const cardWidth = (width - 60) / 2; // 60 = horizontal padding + gap
 
